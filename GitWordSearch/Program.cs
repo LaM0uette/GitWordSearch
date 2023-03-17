882e862495cd24b7b8d40befba8d7fffbf42d825
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-17 11:45:46 +0100

    search all files in all commits

--- GitWordSearch/Program.cs
+++ GitWordSearch/Program.cs
@@ -5,49 +5,42 @@ namespace GitWordSearch;
 
 public static class Program
 {
-    static void Main(string[] args)
+    private static void Main()
     {
-        if (args.Length != 2)
+        const string repoPath = "D:\\__PROG__\\LaMUIette";
+        const string searchText = "partial";
+
+        using var repo = new Repository(repoPath);
+        
+        var regex = new Regex(Regex.Escape(searchText));
+
+        foreach (var commit in repo.Commits)
         {
-            Console.WriteLine("Usage: GitSearchCommits <repository_path> <search_text>");
-            return;
-        }
-
-        string repoPath = args[0];
-        string searchText = args[1];
-
-        using (var repo = new Repository(repoPath))
-        {
-            var regex = new Regex(Regex.Escape(searchText));
-
-            foreach (var commit in repo.Commits)
+            foreach (var parent in commit.Parents)
             {
-                foreach (var parent in commit.Parents)
-                {
-                    var changes = repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree);
+                var changes = repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree);
 
-                    foreach (var change in changes)
+                foreach (var change in changes)
+                {
+                    if (change.Status != ChangeKind.Added && change.Status != ChangeKind.Modified) continue;
+                    
+                    try
                     {
-                        if (change.Status == ChangeKind.Added || change.Status == ChangeKind.Modified)
+                        var blob = (Blob)commit[change.Path].Target;
+                        
+                        using var contentStream = blob.GetContentStream();
+                        using var streamReader = new StreamReader(contentStream);
+                        
+                        var fileContent = streamReader.ReadToEnd();
+                        
+                        if (regex.IsMatch(fileContent))
                         {
-                            try
-                            {
-                                var blob = (Blob)commit[change.Path].Target;
-                                using (var contentStream = blob.GetContentStream())
-                                using (var streamReader = new StreamReader(contentStream))
-                                {
-                                    string fileContent = streamReader.ReadToEnd();
-                                    if (regex.IsMatch(fileContent))
-                                    {
-                                        Console.WriteLine($"Commit {commit.Sha} : {change.Path}");
-                                    }
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Error processing file: {change.Path}\n{ex.Message}");
-                            }
+                            Console.WriteLine($"Commit {commit.Sha} : {change.Path}");
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error processing file: {change.Path}\n{ex.Message}");
                     }
                 }
             }
